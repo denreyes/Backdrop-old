74c10c69b1ba3d028510daef37b63c807849bea3
Dj <denj.rey@gmail.com>
2015-09-19 12:59:38 +0800

    Login Layout

---
+++ app/src/main/java/io/denreyes/backdrop/LoginActivity.java
@@ -0,0 +1,102 @@
+package io.denreyes.backdrop;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.Toast;
+
+import com.facebook.drawee.backends.pipeline.Fresco;
+import com.facebook.drawee.interfaces.DraweeController;
+import com.facebook.drawee.view.SimpleDraweeView;
+import com.spotify.sdk.android.authentication.AuthenticationClient;
+import com.spotify.sdk.android.authentication.AuthenticationRequest;
+import com.spotify.sdk.android.authentication.AuthenticationResponse;
+import com.spotify.sdk.android.player.Config;
+import com.spotify.sdk.android.player.ConnectionStateCallback;
+import com.spotify.sdk.android.player.Player;
+import com.spotify.sdk.android.player.PlayerNotificationCallback;
+import com.spotify.sdk.android.player.PlayerState;
+import com.spotify.sdk.android.player.Spotify;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+import kaaes.spotify.webapi.android.SpotifyApi;
+import kaaes.spotify.webapi.android.SpotifyService;
+import kaaes.spotify.webapi.android.models.TracksPager;
+import retrofit.Callback;
+import retrofit.RetrofitError;
+import retrofit.client.Response;
+
+/**
+ * Created by Dj on 9/19/2015.
+ */
+public class LoginActivity extends AppCompatActivity implements
+        ConnectionStateCallback {
+    @Bind(R.id.img_login_bg)
+    SimpleDraweeView mImgBg;
+
+    private static final String CLIENT_ID = "1fab4aae4d7a40acbb15aa25d1c3bcd1";
+    private static final String REDIRECT_URI = "backdrop-io://callback";
+
+    private Player mPlayer;
+    // Request code that will be used to verify if the result comes from correct activity
+// Can be any integer
+    private static final int REQUEST_CODE = 1337;
+    private static String mTrackId = "";
+    static final Handler MAIN_THREAD = new Handler(Looper.getMainLooper());
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        Fresco.initialize(this);
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        ButterKnife.bind(this);
+
+//        mImgBg.setImageURI(Uri.parse(getString(R.string.login_bg)));
+        DraweeController controller = Fresco.newDraweeControllerBuilder()
+                .setUri(Uri.parse(getString(R.string.login_bg)))
+                .setAutoPlayAnimations(true)
+                .build();
+        mImgBg.setController(controller);
+
+//        AuthenticationRequest.Builder builder = new AuthenticationRequest.Builder(CLIENT_ID,
+//                AuthenticationResponse.Type.TOKEN,
+//                REDIRECT_URI);
+//        builder.setScopes(new String[]{"user-read-private", "streaming"});
+//        AuthenticationRequest request = builder.build();
+//
+//        AuthenticationClient.openLoginActivity(this, REQUEST_CODE, request);
+    }
+
+    @Override
+    public void onLoggedIn() {
+        Log.d("MainActivity", "User logged in");
+        startActivity(new Intent(this, MainActivity.class));
+    }
+
+    @Override
+    public void onLoggedOut() {
+        Log.d("MainActivity", "User logged out");
+    }
+
+    @Override
+    public void onLoginFailed(Throwable error) {
+        Log.d("MainActivity", "Login failed");
+    }
+
+    @Override
+    public void onTemporaryError() {
+        Log.d("MainActivity", "Temporary error occurred");
+    }
+
+    @Override
+    public void onConnectionMessage(String message) {
+        Log.d("MainActivity", "Received connection message: " + message);
+    }
+}
